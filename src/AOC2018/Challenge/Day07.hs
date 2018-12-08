361e742c2e3acd93bcdba163bfb05a6396053ae2
Justin Le <justin@jle.im>
2018-12-07 18:08:10 -0800

    implement things in terms of a scheduler abstarct data type

--- src/AOC2018/Challenge/Day07.hs
+++ src/AOC2018/Challenge/Day07.hs
@@ -14,26 +14,37 @@
 module AOC2018.Challenge.Day07 (
     day07a
   , day07b
+  , Scheduler(..)
+  , Pop(..)
+  , newScheduler
+  , stepScheduler
   ) where
 
-import           AOC2018.Solver       ((:~>)(..), dyno_)
+-- import           AOC2018.Solver       ((:~>)(..), dyno_)
+-- import           Control.Lens
+-- import           Control.Monad        (unless)
+-- import           Control.Monad.State  (StateT, runStateT)
+-- import           Control.Monad.Writer (Writer, execWriter, tell)
+-- import           Data.Bifunctor       (first, second)
+-- import           Data.Char            (ord, isUpper)
+-- import           Data.Foldable        (fold, find, forM_, toList)
+-- import           Data.Map             (Map)
+-- import           Data.Semigroup       (Sum(..))
+-- import           Data.Set             (Set)
+-- import           Data.Set.NonEmpty    (NESet)
+-- import           Data.Tuple           (swap)
+-- import           Data.Witherable      (wither)
+-- import           Numeric.Natural      (Natural)
+import           AOC2018.Prelude
 import           Control.Lens
-import           Control.Monad        (unless)
-import           Control.Monad.State  (StateT, runStateT)
-import           Control.Monad.Writer (Writer, execWriter, tell)
-import           Data.Bifunctor       (first, second)
-import           Data.Char            (ord, isUpper)
-import           Data.Foldable        (fold, find, forM_, toList)
-import           Data.Map             (Map)
-import           Data.Semigroup       (Sum(..))
-import           Data.Set             (Set)
-import           Data.Set.NonEmpty    (NESet)
-import           Data.Tuple           (swap)
-import           Data.Witherable      (wither)
-import           Numeric.Natural      (Natural)
-import qualified Data.Map             as M
-import qualified Data.Set             as S
-import qualified Data.Set.NonEmpty    as NES
+import           Control.Monad.Writer
+import           Data.These
+import qualified Data.List.NonEmpty      as NE
+import qualified Data.Map                as M
+import qualified Data.Map.NonEmpty       as NEM
+import qualified Data.Set                as S
+import qualified Data.Set.NonEmpty       as NES
+
 
 parseAll :: String -> Maybe (Map Char (Set Char))
 parseAll = fmap (M.fromListWith (<>) . (map . second) S.singleton)
@@ -91,73 +102,135 @@ day07a = MkSol
     }
 
 
-data BS2 a = BS2
-    { _bs2Deps    :: Map a (NESet a)
-    , _bs2Active  :: Map a Natural
-    , _bs2Waiting :: Set a
-    }
+-- data BS2 a = BS2
+--     { _bs2Deps    :: Map a (NESet a)
+--     , _bs2Active  :: Map a Natural
+--     , _bs2Waiting :: Set a
+--     }
 
-makeLenses ''BS2
+-- makeLenses ''BS2
 
--- | Tick down all current threads. If any threads finish, take them out of
--- the map and put them into a set of finished results.
-tickAll :: Map a Natural -> (Set a, Map a Natural)
-tickAll = first M.keysSet . M.mapEither tick
-  where
-    tick i
-        | i <= 0    = Left ()
-        | otherwise = Right (i - 1)
+-- buildSleigh
+--     :: Ord a
+--     => Int                -- ^ number of threads
+--     -> Map a (Set a)      -- ^ children map
+--     -> (a -> Natural)     -- ^ initializer
+--     -> StateT (BS2 a) (Writer (Sum Int)) ()
+-- buildSleigh cap childs starter = go
+--   where
+--     go = do
+--       -- tick the clock
+--       tell $ Sum 1
+
+--       -- tick the threads, and get expired items
+--       expired   <- bs2Active %%= tickAll
+
+--       -- remove any expired dependencies from dependencies map
+--       bs2Deps . wither        %= NES.nonEmptySet
+--                                . (`S.difference` expired)
+--                                . NES.toSet
+
+--       -- add the dependencies of expired items to the queue
+--       bs2Waiting              <>= foldMap (fold . (`M.lookup` childs)) expired
+
+--       numToAdd <- uses bs2Active  $ (cap -) . M.size
+--       deps     <- use  bs2Deps
+--       eligible <- uses bs2Waiting $ S.filter (`M.notMember` deps)
+
+--       -- take items from eligible waiting values to fill in the new gaps
+--       let toAdd = S.take numToAdd eligible
+
+--       -- add the items to the active threads
+--       newActive <- bs2Active <<>= M.fromSet starter toAdd
+--       -- delete the newly active items from the queue
+--       bs2Waiting               %= (`S.difference` toAdd)
+
+--       unless (M.null newActive) go
 
 buildSleigh
-    :: Ord a
-    => Int                -- ^ number of threads
-    -> Map a (Set a)      -- ^ children map
-    -> (a -> Natural)     -- ^ initializer
-    -> StateT (BS2 a) (Writer (Sum Int)) ()
-buildSleigh cap childs starter = go
+    :: forall a. Ord a
+    => Int
+    -> (a -> Natural)
+    -> Map a (Set a)
+    -> Maybe Natural
+buildSleigh cap waiter mp = NES.nonEmptySet (findRoots mp) <&> \rts ->
+                              go (flipMap mp) S.empty (newScheduler cap waiter rts)
   where
-    go = do
-      -- tick the clock
-      tell $ Sum 1
+    go  :: Map a (NESet a)
+        -> Set a
+        -> Scheduler a
+        -> Natural
+    go deps toAdd sched0 = case stepScheduler toAdd sched0 of
+      Pop{..} ->
+        let newDeps = flip mapMaybe deps $ NES.nonEmptySet
+                                         . (`NES.difference` _popItems)
+            newAdds = S.filter (`M.notMember` newDeps)
+                    . foldMap (fold . (`M.lookup` mp))
+                    $ _popItems
+        in  _popTime + case _popSched of
+              Just s  -> go newDeps newAdds s
+              Nothing -> case NES.nonEmptySet newAdds of
+                Nothing -> 0
+                Just a  -> go newDeps S.empty $ newScheduler cap waiter a
 
-      -- tick the threads, and get expired items
-      expired   <- bs2Active %%= tickAll
-
-      -- remove any expired dependencies from dependencies map
-      bs2Deps . wither        %= NES.nonEmptySet
-                               . (`S.difference` expired)
-                               . NES.toSet
-
-      -- add the dependencies of expired items to the queue
-      bs2Waiting              <>= foldMap (fold . (`M.lookup` childs)) expired
-
-      numToAdd <- uses bs2Active  $ (cap -) . M.size
-      deps     <- use  bs2Deps
-      eligible <- uses bs2Waiting $ S.filter (`M.notMember` deps)
-
-      -- take items from eligible waiting values to fill in the new gaps
-      let toAdd = S.take numToAdd eligible
-
-      -- add the items to the active threads
-      newActive <- bs2Active <<>= M.fromSet starter toAdd
-      -- delete the newly active items from the queue
-      bs2Waiting               %= (`S.difference` toAdd)
-
-      unless (M.null newActive) go
-
-
-day07b :: Map Char (Set Char) :~> Int
+day07b :: Map Char (Set Char) :~> Natural
 day07b = MkSol
     { sParse = parseAll
     , sShow  = show
-    , sSolve = \mp -> Just $
-        let cap               = dyno_ "cap" 5
-            baseWait          = dyno_ "wait" 60
-            waitTime          = fromIntegral . (+ baseWait) . subtract (ord 'A') . ord
-            (active, waiting) = S.splitAt cap $ findRoots mp
-        in  getSum . execWriter . runStateT (buildSleigh cap mp waitTime) $ BS2
-                { _bs2Deps    = flipMap mp
-                , _bs2Active  = M.fromSet waitTime active
-                , _bs2Waiting = waiting
-                }
+    , sSolve = \mp ->
+        let waitTime          = fromIntegral
+                              . (+ 1)
+                              . (+ dyno_ "wait" 60)
+                              . subtract (ord 'A')
+                              . ord
+        in  buildSleigh (dyno_ "cap" 5) waitTime mp
     }
+
+data Scheduler a = MkSched
+    { _schedQueue  :: !(Set a)
+    , _schedActive :: !(NEMap a Natural)
+    , _schedCap    :: !Int
+    , _schedWaiter :: !(a -> Natural)
+    }
+
+newScheduler :: Int -> (a -> Natural) -> NESet a -> Scheduler a
+newScheduler _schedCap _schedWaiter queue = MkSched{..}
+  where
+    (_schedQueue, _schedActive) = case NES.splitAt _schedCap queue of
+      This addAll -> (S.empty, NEM.fromSet _schedWaiter addAll)
+      That _      -> errorWithoutStackTrace "newScheduler: Capacity should be greater than 0"
+      These toAdd toKeep -> (NES.toSet toKeep, NEM.fromSet _schedWaiter toAdd)
+
+data Pop a = Pop { _popTime  :: !Natural
+                 , _popItems :: !(NESet a)
+                 , _popSched :: !(Maybe (Scheduler a))
+                 }
+
+stepScheduler
+    :: Ord a
+    => Set a
+    -> Scheduler a
+    -> Pop a
+stepScheduler new sched = Pop{..}
+  where
+    newQueue        = _schedQueue sched <> new
+    numToAdd        = _schedCap sched - NEM.size (_schedActive sched)
+    (toAdd, toKeep) = S.splitAt numToAdd newQueue
+    newActives      = NES.withNonEmpty
+        (_schedActive sched)
+        ((<> _schedActive sched) . NEM.fromSet (_schedWaiter sched))
+        toAdd
+    (toPop@((_,_popTime):|_) :| stillActive) = NE.groupWith1 snd
+                                             . NE.sortWith snd
+                                             . NEM.toList
+                                             $ newActives
+    _popItems      = NES.fromDistinctAscList . fmap fst $ toPop
+    shiftedActives = M.map (subtract _popTime)
+                   . M.fromDistinctAscList
+                   . concatMap toList
+                   $ stillActive
+    _popSched      = NEM.nonEmptyMap shiftedActives <&> \a ->
+                       sched { _schedQueue  = toKeep
+                             , _schedActive = a
+                             }
+
