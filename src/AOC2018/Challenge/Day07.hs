c1a6130ddb92f8f46803ed06e2fc86a1b0c8bfbc
Justin Le <justin@jle.im>
2018-12-07 20:51:15 -0800

    rewrote things abck to using explicit recursion for day 7 because it's a little cleaner

--- src/AOC2018/Challenge/Day07.hs
+++ src/AOC2018/Challenge/Day07.hs
@@ -14,16 +14,11 @@
 module AOC2018.Challenge.Day07 (
     day07a
   , day07b
-  -- , Scheduler(..)
-  -- , Pop(..)
-  -- , newScheduler
-  -- , stepScheduler
   ) where
 
 import           AOC2018.Prelude
 import           Control.Lens
 import           Control.Monad.RWS
-import           Control.Monad.Writer
 import qualified Data.List.NonEmpty      as NE
 import qualified Data.Map                as M
 import qualified Data.Set                as S
@@ -42,11 +37,11 @@ parseLine = pack . filter isUpper
     pack _       = Nothing
 
 
-flipMap :: (Ord a, Ord b) => Map a (Set b) -> Map b (NESet a)
-flipMap = M.fromListWith (<>)
-        . map (second NES.singleton . swap)
-        . concatMap (traverse toList)
-        . M.toList
+makeDeps :: (Ord a, Ord b) => Map a (Set b) -> Map b (NESet a)
+makeDeps = M.fromListWith (<>)
+         . map (second NES.singleton . swap)
+         . concatMap (traverse toList)
+         . M.toList
 
 findRoots :: Ord a => Map a (Set a) -> Set a
 findRoots mp = cs `S.difference` targs
@@ -55,36 +50,22 @@ findRoots mp = cs `S.difference` targs
     targs = S.unions $ toList mp
 
 
-data BS1 a = BS1
-    { _bs1Deps   :: Map a (NESet a)
-    , _bs1Active :: Set a
-    }
-
-makeLenses ''BS1
-
-lexicoTopo :: Ord a => Map a (Set a) -> StateT (BS1 a) (Writer [a]) ()
-lexicoTopo childs = go
+lexicoTopo :: Ord a => Map a (Set a) -> [a]
+lexicoTopo childs = go (makeDeps childs) (findRoots childs)
   where
-    go = do
-      deps   <- use bs1Deps
-      active <- use bs1Active
-      forM_ (find (`M.notMember` deps) active) $ \c -> do
-        tell [c]
-        bs1Deps . wither %= NES.nonEmptySet . NES.delete c
-        bs1Active . at c .= Nothing
-        bs1Active       <>= fold (M.lookup c childs)
-        go
+    go deps active = flip foldMap (find (`M.notMember` deps) active) $ \c ->
+      let newDeps   = mapMaybe (NES.nonEmptySet . NES.delete c) deps
+          newActive = maybe id (<>) (M.lookup c childs)
+                    . S.delete c
+                    $ active
+      in  c : go newDeps newActive
 
 day07a :: Map Char (Set Char) :~> String
 day07a = MkSol
     { sParse = parseAll
     , sShow  = id
-    , sSolve = \mp -> Just . execWriter . runStateT (lexicoTopo mp) $ BS1
-          { _bs1Deps   = flipMap mp
-          , _bs1Active = findRoots mp
-          }
+    , sSolve = Just . lexicoTopo
     }
-
 
 
 data Env a = Env { _envCap    :: Int
@@ -100,42 +81,32 @@ data Scheduler a = MkSched
 
 makeClassy ''Scheduler
 
-data BS2 a = BS2
-    { _bs2Deps      :: Map a (NESet a)
-    , _bs2Scheduler :: Scheduler a
-    }
-
-makeLenses ''BS2
-
-instance HasScheduler (BS2 a) a where
-    scheduler = bs2Scheduler
-
 buildSleigh
     :: forall a m.
        ( Ord a
-       , MonadState  (BS2 a) m
+       , MonadState  (Scheduler a) m
        , MonadReader (Env a) m
        , MonadWriter (Sum Natural) m
        )
     => Map a (Set a)
     -> m ()
-buildSleigh childs = go (findRoots childs)
+buildSleigh childs = go (findRoots childs) (makeDeps childs)
   where
-    go toAdd = do
+    go :: Set a -> Map a (NESet a) -> m ()
+    go toAdd deps = do
       popped <- stepScheduler toAdd
-      forM_ (NES.nonEmptySet popped) $ \popped' -> do
-        bs2Deps . wither %= NES.nonEmptySet . (`NES.difference` popped')
-        deps   <- use bs2Deps
-
-        go . S.filter (`M.notMember` deps)
-           . foldMap (fold . (`M.lookup` childs))
-           $ popped'
+      forM_ (NES.nonEmptySet popped) $ \popped' ->
+        let newDeps  = mapMaybe (NES.nonEmptySet . (`NES.difference` popped')) deps
+            newToAdd = S.filter (`M.notMember` newDeps)
+                     . foldMap (fold . (`M.lookup` childs))
+                     $ popped'
+        in  go newToAdd newDeps
 
 day07b :: Map Char (Set Char) :~> Natural
 day07b = MkSol
     { sParse = parseAll
     , sShow  = show
-    , sSolve = \mp ->
+    , sSolve = \mp -> Just $
         let env = Env
               { _envCap    = dyno_ "cap" 5
               , _envWaiter = fromIntegral
@@ -144,10 +115,7 @@ day07b = MkSol
                            . subtract (ord 'A')
                            . ord
               }
-        in  Just . getSum . view _3 . runRWS (buildSleigh mp) env $ BS2
-              { _bs2Deps      = flipMap mp
-              , _bs2Scheduler = emptyScheduler
-              }
+        in  getSum . view _3 . runRWS (buildSleigh mp) env $ emptyScheduler
     }
 
 
