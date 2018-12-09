be428f0904f3eadc02b3f756310bf407edc9e2d0
Justin Le <justin@jle.im>
2018-12-08 22:24:16 -0800

    cleaning up imports in day 9

--- src/AOC2018/Challenge/Day09.hs
+++ src/AOC2018/Challenge/Day09.hs
@@ -8,61 +8,61 @@
 -- Portability : non-portable
 --
 -- Day 9.  See "AOC2018.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC2018.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC2018.Challenge.Day09 (
     day09a
   , day09b
   ) where
 
-import           AOC2018.Prelude
-import           Control.Lens
+import           AOC2018.Solver                 ((:~>)(..))
+import           Control.Lens                   (ix, (+~))
+import           Data.Function                  ((&))
+import           Data.List                      (foldl')
 import           Data.List.PointedList.Circular (PointedList(..))
+import           Data.Maybe                     (mapMaybe, fromJust)
+import           Text.Read                      (readMaybe)
 import qualified Data.List.PointedList.Circular as PL
 import qualified Data.Vector.Unboxed            as V
 
 place
     :: Int                       -- ^ number to place
     -> PointedList Int           -- ^ tape
-    -> (PointedList Int, Int)    -- ^ resulting tape, and scored points
+    -> (Int, PointedList Int)    -- ^ resulting tape, and scored points
 place x l
     | x `mod` 23 == 0
-    =       let l'       = PL.moveN (-7) l
-                toAdd    = _focus l'
-                Just l'' = PL.deleteRight l'
-            in  (l'', toAdd + x)
+    =   let l'       = PL.moveN (-7) l
+            toAdd    = _focus l'
+        in  (toAdd + x, fromJust (PL.deleteRight l'))
     | otherwise
-    = ((PL.insertLeft x . PL.moveN 2) l, 0)
+    = (0, (PL.insertLeft x . PL.moveN 2) l)
 
 run :: Int -> Int -> V.Vector Int
-run numPlayers numList = fst $ foldl' go (v0, PL.singleton 0) (zip players toInsert)
+run numPlayers numList = fst
+                       . foldl' go (v0, PL.singleton 0)
+                       $ zip players toInsert
   where
     v0 = V.replicate numPlayers 0
-    go (scores, tp) (p, i) = case place i tp of
-                               (tp', pts) -> (scores & ix p +~ pts, tp')
+    go (!scores, !tp) (!p, !i) = (scores & ix p +~ pts, tp')
+      where
+        (pts, tp') = place i tp
     players  = (`mod` numPlayers) <$> [0 ..]
     toInsert = [1..numList]
 
 day09a :: (Int, Int) :~> Int
 day09a = MkSol
-    { sParse = (\[p,n] -> Just (p, n)) . mapMaybe readMaybe . words
+    { sParse = parse
     , sShow  = show
-    , sSolve = \(p, n) -> Just . V.maximum $ run p n
+    , sSolve = Just . V.maximum . uncurry run 
     }
 
 day09b :: _ :~> _
 day09b = MkSol
-    { sParse = (\[p,n] -> Just (p, n)) . mapMaybe readMaybe . words
+    { sParse = parse
     , sShow  = show
-    , sSolve = \(p, n) -> Just . V.maximum $ run p (n * 100)
+    , sSolve = Just . V.maximum . uncurry run
     }
+
+parse :: String -> Maybe (Int, Int)
+parse xs = case mapMaybe readMaybe (words xs) of
+    [p, n] -> Just (p, n)
+    _      -> Nothing
