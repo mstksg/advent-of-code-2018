421d1ffc1d9dc4abaa8aa35591ae331e2461a2f7
Justin Le <justin@jle.im>
2018-12-08 21:49:14 -0800

    initial day 9

--- src/AOC2018/Challenge/Day09.hs
+++ src/AOC2018/Challenge/Day09.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC2018.Challenge.Day09
 -- Copyright   : (c) Justin Le 2018
@@ -29,17 +26,43 @@ module AOC2018.Challenge.Day09 (
   ) where
 
 import           AOC2018.Prelude
+import           Control.Lens
+import           Data.List.PointedList.Circular (PointedList(..))
+import qualified Data.List.PointedList.Circular as PL
+import qualified Data.Vector.Unboxed            as V
 
-day09a :: _ :~> _
+place
+    :: Int                       -- ^ number to place
+    -> PointedList Int           -- ^ tape
+    -> (PointedList Int, Int)    -- ^ resulting tape, and scored points
+place x l
+    | x `mod` 23 == 0
+    =       let l'       = PL.moveN (-7) l
+                toAdd    = _focus l'
+                Just l'' = PL.deleteRight l'
+            in  (l'', toAdd + x)
+    | otherwise
+    = ((PL.insertLeft x . PL.moveN 2) l, 0)
+
+run :: Int -> Int -> V.Vector Int
+run numPlayers numList = fst $ foldl' go (v0, PL.singleton 0) (zip players toInsert)
+  where
+    v0 = V.replicate numPlayers 0
+    go (scores, tp) (p, i) = case place i tp of
+                               (tp', pts) -> (scores & ix p +~ pts, tp')
+    players  = (`mod` numPlayers) <$> [0 ..]
+    toInsert = [1..numList]
+
+day09a :: (Int, Int) :~> Int
 day09a = MkSol
-    { sParse = Just
-    , sShow  = id
-    , sSolve = Just
+    { sParse = (\[p,n] -> Just (p, n)) . mapMaybe readMaybe . words
+    , sShow  = show
+    , sSolve = \(p, n) -> Just . V.maximum $ run p n
     }
 
 day09b :: _ :~> _
 day09b = MkSol
-    { sParse = Just
-    , sShow  = id
-    , sSolve = Just
+    { sParse = (\[p,n] -> Just (p, n)) . mapMaybe readMaybe . words
+    , sShow  = show
+    , sSolve = \(p, n) -> Just . V.maximum $ run p (n * 100)
     }
