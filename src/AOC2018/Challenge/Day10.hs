032c286679f80f8036b74a65cfce284a6bf578d7
Justin Le <justin@jle.im>
2018-12-09 22:26:50 -0800

    clean up imports for day 10

--- src/AOC2018/Challenge/Day10.hs
+++ src/AOC2018/Challenge/Day10.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC2018.Challenge.Day10
 -- Copyright   : (c) Justin Le 2018
@@ -11,29 +8,20 @@
 -- Portability : non-portable
 --
 -- Day 10.  See "AOC2018.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC2018.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
-module AOC2018.Challenge.Day10 where
-    -- day10a
-  -- , day10b
-  -- ) where
+module AOC2018.Challenge.Day10 (
+    day10a
+  , day10b
+  ) where
 
-import           AOC2018.Prelude
-import           Data.Semigroup.Foldable
-import qualified Data.List.NonEmpty      as NE
-import qualified Data.Map                as M
-import qualified Data.Set                as S
-import qualified Linear                  as L
+import           AOC2018.Solver     ((:~>)(..))
+import           AOC2018.Util       (clearOut)
+import           Data.Char          (isDigit)
+import           Data.Foldable      (toList)
+import           Data.Semigroup     (Min(..), Max(..))
+import           Data.Set           (Set)
+import           Linear             (V2(..))
+import qualified Data.Set           as S
 
 type Point = V2 Int
 
