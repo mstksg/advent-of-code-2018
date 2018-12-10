bde0e8e41bf7e4ff406155283ae25c88c7d7318b
Justin Le <justin@jle.im>
2018-12-10 15:14:49 -0800

    closed form solution for day 10

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -14,6 +14,7 @@
 module AOC.Challenge.Day10 (
     day10a
   , day10b
+  , centralize
   ) where
 
 import           AOC.Common     (clearOut)
@@ -25,45 +26,40 @@ import           Data.Foldable  (toList, foldMap)
 import           Data.List      (unfoldr, uncons)
 import           Data.Map       (Map)
 import           Data.Maybe     (catMaybes)
-import           Data.Semigroup (Min(..), Max(..))
+import           Data.Semigroup (Min(..), Max(..), Sum(..))
 import           Data.Set       (Set)
 import           Linear         (V2(..))
 import           Text.Heredoc   (here)
 import qualified Data.Map       as M
 import qualified Data.Set       as S
+import qualified Linear         as L
 
-type Point = V2 Int
+type Point   = V2 Double
+type Lattice = V2 Int
 
-simulate
-    :: [Point]        -- ^ velocities
-    -> [Point]        -- ^ points
-    -> [Point]        -- ^ new points
-simulate = zipWith (+)
-
-boundingBox :: [Point] -> V2 Point
-boundingBox ps = V2 xMin yMin `V2` V2 xMax yMax
+-- | Shift so that centroid is at zero
+centralize :: [Point] -> [Point]
+centralize ps = map (subtract mean) ps
   where
-    (Min xMin, Min yMin, Max xMax, Max yMax) = flip foldMap ps $ \(V2 x y) ->
-        (Min x, Min y, Max x, Max y)
+    mean = (/ len) <$> tot
+    (Sum tot, Sum len) = foldMap (\x -> (Sum x, Sum 1)) ps
 
-clusterArea :: [Point] -> Int
-clusterArea (boundingBox -> V2 mins maxs) = product $ maxs - mins
+-- | Multiply and find trace
+traceMul :: [Point] -> [Point] -> Double
+traceMul xs ys = sum $ zipWith L.dot xs ys
 
 findWord
-    :: [Point]            -- ^ velocities
-    -> [Point]            -- ^ points
-    -> (Set Point, Int)   -- ^ points in word, and # of iterations
-findWord vs xs0 = go 0 (clusterArea xs0) xs0
+    :: [Point]              -- ^ velocities
+    -> [Point]              -- ^ points
+    -> (Set Lattice, Int)   -- ^ points in word, and # of iterations
+findWord vs xs = (S.fromList ((map . fmap) round final), round t)
   where
-    go :: Int -> Int -> [Point] -> (Set Point, Int)
-    go !i !area !xs
-        | area' > area = (S.fromList xs, i)
-        | otherwise    = go (i + 1) area' xs'
-      where
-        xs'   = simulate vs xs
-        area' = clusterArea xs'
+    vs'   = centralize vs
+    xs'   = centralize xs
+    t     = negate $ traceMul xs' vs' / traceMul vs' vs'
+    final = zipWith (\v x -> x + t L.*^ v) vs xs
 
-day10a :: ([Point], [Point]) :~> Set Point
+day10a :: ([Point], [Point]) :~> Set Lattice
 day10a = MkSol
     { sParse = fmap unzip . traverse parsePoint . lines
     , sShow  = parseResult
@@ -77,19 +73,15 @@ day10b = MkSol
     , sSolve = Just . snd . uncurry findWord
     }
 
--- | Old solution: Display the points as ASCII plot
-_display :: Set Point -> String
-_display ps = unlines [ [ if V2 x y `S.member` ps then '#' else '.'
-                       | x <- [xMin .. xMax]
-                       ]
-                     | y <- [yMin .. yMax]
-                     ]
+boundingBox :: [Lattice] -> V2 Lattice
+boundingBox ps = V2 xMin yMin `V2` V2 xMax yMax
   where
-    V2 xMin yMin `V2` V2 xMax yMax = boundingBox (toList ps)
+    (Min xMin, Min yMin, Max xMax, Max yMax) = flip foldMap ps $ \(V2 x y) ->
+        (Min x, Min y, Max x, Max y)
 
 -- | New solution: Parse the set of points into a string, based on
 -- 'letterforms'.
-parseResult :: Set Point -> String
+parseResult :: Set Lattice -> String
 parseResult ps = case M.lookup letter letterMap of
     Nothing -> []
     Just c  -> c : parseResult rest
@@ -108,13 +100,13 @@ parsePoint xs = case map read . words . clearOut p $ xs of
 
 -- | A map of a set of "on" points (for a 6x10 grid) to the letter
 -- they represent
-letterMap :: Map (Set Point) Char
+letterMap :: Map (Set Lattice) Char
 letterMap = M.fromList
           . unfoldr (uncurry peel)
           . second (filter (not . null) . lines)
           $ letterforms
   where
-    peel :: String -> [String] -> Maybe ((Set Point, Char), (String, [String]))
+    peel :: String -> [String] -> Maybe ((Set Lattice, Char), (String, [String]))
     peel cs ls = do
       (d,ds) <- uncons cs
       let (m,ms) = unzip . map (splitAt 6) $ ls
