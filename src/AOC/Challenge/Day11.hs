269fb82be9ffd781430aefe80f0b307bb9c7b9b8
Justin Le <justin@jle.im>
2018-12-12 02:22:18 -0800

    so cut-off works now

--- src/AOC/Challenge/Day11.hs
+++ src/AOC/Challenge/Day11.hs
@@ -12,24 +12,19 @@
 module AOC.Challenge.Day11 (
     day11a
   , day11b
-  , chunkyVars
-  , mkMap
   ) where
 
--- import           AOC.Common                    (foldMapPar1)
--- import           AOC.Solver                    ((:~>)(..))
--- import           Control.DeepSeq               (force)
--- import           Control.Monad                 ((<=<))
--- import           Data.Foldable                 (maximumBy)
--- import           Data.Ix                       (range)
--- import           Data.Map                      (Map)
--- import           Data.Maybe                    (catMaybes)
--- import           Data.Ord                      (comparing)
--- import           Data.Semigroup                (Max(..), Arg(..))
--- import           Linear                        (V2(..))
--- import           Text.Read                     (readMaybe)
--- import qualified Data.List.NonEmpty            as NE
-import           AOC.Prelude
+import           AOC.Common                       (meanVar)
+import           AOC.Solver                       ((:~>)(..))
+import           Control.DeepSeq                  (force)
+import           Data.Foldable                    (maximumBy)
+import           Data.Ix                          (range)
+import           Data.Map                         (Map)
+import           Data.Maybe                       (catMaybes)
+import           Data.Ord                         (comparing)
+import           Data.Profunctor                  (dimap)
+import           Linear                           (V2(..))
+import           Text.Read                        (readMaybe)
 import qualified Control.Foldl                    as F
 import qualified Data.Map                         as M
 import qualified Data.Set                         as S
@@ -58,16 +53,6 @@ findMaxThree mp = fst
 mkMap :: Int -> Map Point Int
 mkMap i = M.fromSet (powerLevel i) . S.fromList $ range (V2 1 1, V2 300 300)
 
-chunkyVars :: Map Point Int -> Map Int Double
-chunkyVars mp = flip M.fromSet (S.fromList [1..300]) $ \n ->
-    F.fold (dimap fromIntegral snd meanVar)
-      [ fromPartialSums ps p n
-      | p <- range (V2 1 1, V2 (300 - n + 1) (300 - n + 1))
-      ]
-  where
-    !ps = partialSums mp
-
-
 day11a :: Int :~> Point
 day11a = MkSol
     { sParse = readMaybe
@@ -86,9 +71,9 @@ findMaxAny :: Map Point Int -> (Point, Int)
 findMaxAny mp = fst $ go 1
   where
     go n
-       | goOn > 0.0001 = maximumBy (comparing snd) [((pMax, n), oMax), go (n + 1)]
-       | otherwise = -- traceShow (n, oMax, goOn)
-                     ((pMax, n), oMax)
+       | goOn > 0.01 = maximumBy (comparing snd) [((pMax, n), oMax), go (n + 1)]
+       | otherwise   = ((pMax, n), oMax)
+       -- = traceShow (n, oMax, goOn) ((pMax, n), oMax)
       where
         (pMax, oMax) = maximumBy (comparing snd)
             [ (p, fromIntegral (fromPartialSums ps p n))
@@ -98,25 +83,18 @@ findMaxAny mp = fst $ go 1
             [ probGreaterThan oMax n'
             | n' <- [n + 1 .. 300]
             ]
-    σ2 = F.fold (dimap fromIntegral snd meanVar) mp
+    (sqrt->σ) = F.fold (dimap fromIntegral snd meanVar) mp
     !ps = partialSums mp
     probGreaterThan o n
         | prob2 == 0 = 0
         | otherwise  = probIn2
       where
         n' = fromIntegral n
-        distr2  = D.normalDistr (-(n' ** 2)) . sqrt . sum $
-                    [ (n' ** 2) * σ2
-                    , 2 * fromIntegral (choose2 n) * σ2 * ρ
-                    ]
-        -- TODO: this probably should be figured out
-        ρ = 1 - (n' ** 2 - (n' - 1) ** 2) / (n' ** 2)
+        distr2  = D.normalDistr (-(n' ** 2) * 0.5) . (* σ) $
+                    n' * (1 - ((n' - 1) / 300) ** 3)    -- TODO: find the actual stdev
         prob2   = D.complCumulative distr2 o
         numIn2  = (300 - n + 1)^(2 :: Int)
         probIn2 = 1 - D.probability (D.binomial numIn2 prob2) 0
-
-choose2 :: Int -> Int
-choose2 n = (n * (n - 1)) `div` 2
 
 fromPartialSums :: Map Point Int -> Point -> Int -> Int
 fromPartialSums ps (subtract (V2 1 1)->p) n = sum . catMaybes $
@@ -135,3 +113,15 @@ partialSums mp = force pasu
       ,            M.lookup (p0 - V2 1 0) pasu
       ,            M.lookup (p0 - V2 0 1) pasu
       ]
+
+-- | Debug: find the variance of the map at every square size
+_chunkyVars :: Map Point Int -> Map Int Double
+_chunkyVars mp = flip M.fromSet (S.fromList [1..300]) $ \n ->
+    F.fold (dimap fromIntegral snd meanVar)
+      [ fromPartialSums ps p n
+      | p <- range (V2 1 1, V2 (300 - n + 1) (300 - n + 1))
+      ]
+  where
+    !ps = partialSums mp
+
+
