1a845db0a7062cf45978d63bf2da68d8ff0d2bf3
Justin Le <justin@jle.im>
2018-12-18 22:49:45 -0800

    day 19, but part 2 was done by hand

--- src/AOC/Challenge/Day19.hs
+++ src/AOC/Challenge/Day19.hs
@@ -24,22 +24,121 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day19 (
-    -- day19a
-  -- , day19b
+    day19a
+  , day19b
   ) where
 
 import           AOC.Prelude
+import           Control.Lens hiding        (uncons)
+import           Data.Bitraversable
+import           Data.Bits
+import           Data.Vector.Unboxed.Sized  (Vector)
+import qualified Data.Map                   as M
+import qualified Data.Vector.Unboxed.Sized  as V
+import qualified Text.Megaparsec            as P
+import qualified Text.Megaparsec.Char       as P
+import qualified Text.Megaparsec.Char.Lexer as P
+
+type Reg = Vector 6 Int
+
+type Memory = Map Int Instr
+
+-- data ProgState = PS { _psInstr :: Int
+--                     , _psReg   :: Reg
+--                     , _psMem   :: Memory
+--                     }
+
+-- makeLenses ''ProgState
+
+data Instr = I { _iOp  :: OpCode
+               , _iInA :: Finite 6
+               , _iInB :: Int
+               , _iOut :: Finite 6
+               }
+  deriving (Show, Eq, Ord)
+
+data OpCode = OAddR | OAddI
+            | OMulR | OMulI
+            | OBanR | OBanI
+            | OBorR | OBorI
+            | OSetR | OSetI
+            | OGtIR | OGtRI | OGtRR
+            | OEqIR | OEqRI | OEqRR
+  deriving (Show, Eq, Ord, Enum, Bounded)
+
+runOp :: Instr -> Reg -> Reg
+runOp I{..} = case _iOp of
+    OAddR -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA  +  r ^. V.ix (fromIntegral _iInB)
+    OAddI -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA  +  _iInB
+    OMulR -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA  *  r ^. V.ix (fromIntegral _iInB)
+    OMulI -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA  *  _iInB
+    OBanR -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA .&. r ^. V.ix (fromIntegral _iInB)
+    OBanI -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA .&. _iInB
+    OBorR -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA .|. r ^. V.ix (fromIntegral _iInB)
+    OBorI -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA .|. _iInB
+    OSetR -> \r -> r & V.ix _iOut .~ r ^. V.ix _iInA
+    OSetI -> \r -> r & V.ix _iOut .~                     fromIntegral _iInA
+    OGtIR -> \r -> r & V.ix _iOut . enum .~ (fromIntegral _iInA  > r ^. V.ix (fromIntegral _iInB)   )
+    OGtRI -> \r -> r & V.ix _iOut . enum .~ (r ^. V.ix _iInA     > _iInB)
+    OGtRR -> \r -> r & V.ix _iOut . enum .~ (r ^. V.ix _iInA     > r ^. V.ix (fromIntegral _iInB)   )
+    OEqIR -> \r -> r & V.ix _iOut . enum .~ (fromIntegral _iInA == r ^. V.ix (fromIntegral _iInB)   )
+    OEqRI -> \r -> r & V.ix _iOut . enum .~ (r ^. V.ix _iInA    == _iInB)
+    OEqRR -> \r -> r & V.ix _iOut . enum .~ (r ^. V.ix _iInA    == r ^. V.ix (fromIntegral _iInB)   )
+
+stepMemory
+    :: Finite 6
+    -> Memory
+    -> Reg
+    -> Maybe Reg
+stepMemory iPtr mem r0 = M.lookup i mem <&> \instr ->
+    runOp instr r0 & V.ix iPtr +~ 1
+  where
+    i = r0 ^. V.ix iPtr
 
 day19a :: _ :~> _
 day19a = MkSol
-    { sParse = Just
-    , sShow  = id
-    , sSolve = Just
+    { sParse = P.parseMaybe memParser
+    , sShow  = show
+    , sSolve = \(i, m) -> Just
+                        . V.head
+                        . last
+                        . iterateMaybe (stepMemory i m)
+                        $ V.replicate 0
     }
 
 day19b :: _ :~> _
 day19b = MkSol
-    { sParse = Just
-    , sShow  = id
-    , sSolve = Just
+    { sParse = P.parseMaybe memParser
+    , sShow  = show
+    , sSolve = \(i, m) -> Just
+                        . V.head
+                        . last
+                        -- . last
+                        . take 1000
+                        . iterateMaybe (stepMemory i m . traceShowId)
+                        . set (V.ix 0) 1
+                        $ V.replicate 0
     }
+
+
+type Parser = P.Parsec Void String
+
+memParser :: Parser (Finite 6, Memory)
+memParser = (,) <$> (P.string "#ip " `P.between` P.newline) parseFinite
+                <*> (M.fromList . zip [0..] <$> (instrParser `P.sepEndBy1` P.newline))
+
+instrParser :: Parser Instr
+instrParser = I <$> parseOpCode <* P.char ' '
+                <*> parseFinite <* P.char ' '
+                <*> P.decimal   <* P.char ' '
+                <*> parseFinite <* P.skipMany (P.satisfy (/= '\n'))
+  where
+    parseOpCode = P.choice . flip map [OAddR ..] $ \o ->
+        o <$ P.try (P.string (map toLower . drop 1 . show $ o))
+    -- parseComment = do
+    --     P.space1
+    --     P.char '#'
+    --     P.skipMany . P.try $ P.satisfy (/= '\n')
+
+parseFinite :: Parser (Finite 6)
+parseFinite = maybe (fail "number out of range") pure . packFinite =<< P.decimal
