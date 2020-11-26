c589855c48ead3d3d2c8502553ed7ef100997c3b
Justin Le <justin@jle.im>
2020-11-25 22:08:34 -0800

    use advent-of-code-ocr

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -17,23 +17,18 @@ module AOC.Challenge.Day10 (
   , centralize
   ) where
 
-import           AOC.Common        (clearOut, boundingBox)
-import           AOC.Solver        ((:~>)(..))
-import           Control.Monad     (guard)
-import           Data.Bifunctor    (second)
-import           Data.Char         (isDigit)
-import           Data.Foldable     (foldMap)
-import           Data.List         (unfoldr, uncons)
-import           Data.Map          (Map)
-import           Data.Maybe        (catMaybes)
-import           Data.Semigroup    (Sum(..))
-import           Data.Set          (Set)
-import           Linear            (V2(..))
-import           Text.Heredoc      (here)
-import qualified Data.Map          as M
-import qualified Data.Set          as S
-import qualified Data.Set.NonEmpty as NES
-import qualified Linear            as L
+import           AOC.Common     (clearOut)
+import           AOC.Solver     ((:~>)(..))
+import           Advent.OCR     (parseLettersWith)
+import           Control.Lens   (view)
+import           Data.Char      (isDigit)
+import           Data.Foldable  (foldMap)
+import           Data.Maybe     (fromMaybe)
+import           Data.Semigroup (Sum(..))
+import           Data.Set       (Set)
+import           Linear         (V2(..), _x, _y)
+import qualified Data.Set       as S
+import qualified Linear         as L
 
 type Point   = V2 Double
 type Lattice = V2 Int
@@ -62,7 +57,7 @@ findWord (centralize->vs) (centralize->xs) =
 day10a :: ([Point], [Point]) :~> Set Lattice
 day10a = MkSol
     { sParse = fmap unzip . traverse parsePoint . lines
-    , sShow  = parseResult
+    , sShow  = fromMaybe "" . parseLettersWith (view _x) (view _y)
     , sSolve = Just . fst . uncurry findWord
     }
 
@@ -73,17 +68,6 @@ day10b = MkSol
     , sSolve = Just . snd . uncurry findWord
     }
 
--- | New solution: Parse the set of points into a string, based on
--- 'letterforms'.
-parseResult :: Set Lattice -> String
-parseResult ps = case M.lookup letter letterMap of
-    Nothing -> []
-    Just c  -> c : parseResult rest
-  where
-    origin `V2` _  = boundingBox . NES.unsafeFromSet $ ps
-    shiftedPs      = subtract origin `S.map` ps
-    (letter, rest) = S.partition (\(V2 x y) -> x < 6 && y < 10) shiftedPs
-
 parsePoint :: String -> Maybe (Point, Point)
 parsePoint xs = case map read . words . clearOut p $ xs of
     [x,y,vx,vy] -> Just (V2 vx vy, V2 x y)
@@ -91,40 +75,3 @@ parsePoint xs = case map read . words . clearOut p $ xs of
   where
     p '-' = False
     p c   = not $ isDigit c
-
--- | A map of a set of "on" points (for a 6x10 grid) to the letter
--- they represent
-letterMap :: Map (Set Lattice) Char
-letterMap = M.fromList
-          . unfoldr (uncurry peel)
-          . second (filter (not . null) . lines)
-          $ letterforms
-  where
-    peel :: String -> [String] -> Maybe ((Set Lattice, Char), (String, [String]))
-    peel cs ls = do
-      (d,ds) <- uncons cs
-      let (m,ms) = unzip . map (splitAt 6) $ ls
-          pointMap = S.fromList
-                   . foldMap catMaybes
-                   . zipWith (\j -> zipWith (\i c -> V2 i j <$ guard (c == '#'))
-                                            [0..]
-                             )
-                             [0..]
-                   $ m
-      pure ((pointMap, d), (ds, ms))
-
--- | All known letterforms.  Based on
--- <https://gist.github.com/usbpc/5fa0be48ad7b4b0594b3b8b029bc47b4>.
-letterforms :: (String, String)
-letterforms = ("ABCEFGHJKLNPRXZ",[here|
-..##..#####..####.############.####.#....#...####....##.....#....######.#####.#....#######
-.#..#.#....##....##.....#.....#....##....#....#.#...#.#.....##...##....##....##....#.....#
-#....##....##.....#.....#.....#.....#....#....#.#..#..#.....##...##....##....#.#..#......#
-#....##....##.....#.....#.....#.....#....#....#.#.#...#.....#.#..##....##....#.#..#.....#.
-#....######.#.....#####.#####.#.....######....#.##....#.....#.#..######.#####...##.....#..
-#######....##.....#.....#.....#..####....#....#.##....#.....#..#.##.....#..#....##....#...
-#....##....##.....#.....#.....#....##....#....#.#.#...#.....#..#.##.....#...#..#..#..#....
-#....##....##.....#.....#.....#....##....##...#.#..#..#.....#...###.....#...#..#..#.#.....
-#....##....##....##.....#.....#...###....##...#.#...#.#.....#...###.....#....##....##.....
-#....######..####.#######......###.##....#.###..#....########....##.....#....##....#######
-|])
